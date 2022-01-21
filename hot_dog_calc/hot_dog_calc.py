822ec54d4c954f5fd0fff10b5f5db6c9a38713a5
IncomingWill <98003073+IncomingWill@users.noreply.github.com>
2022-01-21 12:52:35 -0600

    Update hot_dog_calc.py

--- hot_dog_calc/hot_dog_calc.py
+++ hot_dog_calc/hot_dog_calc.py
@@ -4,33 +4,54 @@
 # P. 163, Exercise 8, Hot Dog Cookout Calculator Program
 # 01.18.22
 
-# This program will take input and display supply details for a HD cookout
+# This program will take input and display supply details for a Hot Dog Cookout
 
-# Imports for math.ceil() function
-import math
+# Imports
+import math                             # For math.ceil() function
+import sys                              # For Command-Line Args
 
 # CONSTANTS
 
-DOGS_PER_PACKAGE = 10
-BUNS_PER_PACKAGE = 8
+DOGS_PER_PACKAGE = 10                   # Total hot dogs per package      
+BUNS_PER_PACKAGE = 8                    # Total buns per package
 
-# User input
+# Variable Declaration
 
-guestCount = int(input('How many guests are you expecting at the cookout? '))
-dogsPerPerson = int(input('How many whole hot dogs will you prepare for each guest? '))
+    # Variables for user input
+
+guestCount = int(sys.argv[1])           # Number of guests to attend
+dogsPerPerson = int(sys.argv[2])        # Number of hot dogs to server per guest
+guestNoBuns = int(sys.argv[3])          # Number of guests who will not eat buns
+
+    # Variables for calculation
+
+noBuns = 0                              # Total number of buns omitted, guestNoBuns * dogsPerPerson
+totalDogs = 0                           # Total hot dogs to be prepared, guestCount * dogsPerPerson
+totalDogPackages = 0                    # Total Hot Dog Packages needed for purchase, (totalDogs / DOGS_PER_PACKAGE) round up whole package
+totalBuns = 0                           # Total Buns needed, totalDogs - noBuns
+totalBunPackages = 0                    # Total Bun Packages needed, (totalBuns / BUNS_PER_PACKAGE) round up whole package
+leftoverDogs = 0                        # Total leftover hotdogs, totalDogs modulus DOGS_PER_PACKAGE
+leftoverBuns = 0                        # Total leftover buns, (BUNS_PER_PACKAGE * totalBunPackages) modulus totalBuns
+
+# User input -- commented out so command line arguments can be used instead
+
+#guestCount = int(input('How many guests are you expecting at the cookout? '))
+#dogsPerPerson = int(input('How many whole hot dogs will you prepare for each guest? '))
 
     # How many guests will not have a bun?
-guestNoBuns = int(input('How many guests will not have a bun due to allergies or dietary restrictions? '))
+
+#guestNoBuns = int(input('How many guests will not have a bun due to allergies or dietary restrictions? '))
 
     # Number to subtract based on guests with no buns and how many hotdogs will be prepared for them
+
 noBuns = guestNoBuns * dogsPerPerson
 
 # Calculations -> Use math.ceil() function to round up for needed packages
 
 totalDogs = guestCount * dogsPerPerson
-totalDogPackages = (totalDogs // DOGS_PER_PACKAGE)
+totalDogPackages = math.ceil(totalDogs / DOGS_PER_PACKAGE)
 totalBuns = totalDogs - noBuns
-totalBunPackages = (totalBuns // BUNS_PER_PACKAGE)
+totalBunPackages = math.ceil(totalBuns / BUNS_PER_PACKAGE)
 leftoverDogs = totalDogs % DOGS_PER_PACKAGE
 leftoverBuns = (BUNS_PER_PACKAGE * totalBunPackages) % totalBuns
 
@@ -46,3 +67,23 @@ print(f'Total Hot Dog Bun Packages Needed: {totalBunPackages}')
 print(f'Total Leftover Hot Dogs: {leftoverDogs}')
 print(f'Total Leftover Hot Dog Buns: {leftoverBuns}')
 
+#Demonstration of command line arguments borrowed from geeksforgeeks.org
+
+# total arguments
+n = len(sys.argv)
+print("Total arguments passed:", n)
+ 
+# Arguments passed
+print("\nName of Python script:", sys.argv[0])
+ 
+print("\nArguments passed:", end = " ")
+for i in range(1, n):
+    print(sys.argv[i], end = " ")
+     
+# Addition of numbers
+Sum = 0
+# Using argparse module
+for i in range(1, n):
+    Sum += int(sys.argv[i])
+     
+print("\n\nResult:", Sum)
